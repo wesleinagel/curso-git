fbbbf224ce8ca91821497b833e4bbb2d4bf7bd0e
Weslei Nagel <wesleinagel23@hotmail.com>
2021-06-02 14:39:58 -0300

    Ascentada uma mensagem ed bom dia

--- ProjetoGit/Program.cs
+++ ProjetoGit/Program.cs
@@ -8,15 +8,17 @@ namespace ProjetoGit
         // verde: arquivo pronto para ser "commitados " (stage)
 
         /*git add - colocar arquivos modificados na área de "stage"
+         *  git status - verificar quais são os arquivos no "stage" ou e "untracked"
          * 
-         * 
+         * git commit -m "nome do arquivos"
+         * git log
          */
 
 
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
-
+            Console.WriteLine(" Bom dia!");
             Console.ReadLine();
 
         }
