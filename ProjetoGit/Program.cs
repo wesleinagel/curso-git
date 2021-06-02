50b29911716ab335a08290a20fe9204cbe6c85b4
Weslei Nagel <wesleinagel23@hotmail.com>
2021-06-02 14:36:24 -0300

    Projeto iniciado, mostrnaod a mengagem Ola Mundo

---
+++ ProjetoGit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProjetoGit
+{
+    class Program
+    {
+        // vermelho: arquivos não rastreados (untracked)
+        // verde: arquivo pronto para ser "commitados " (stage)
+
+        /*git add - colocar arquivos modificados na área de "stage"
+         * 
+         * 
+         */
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
